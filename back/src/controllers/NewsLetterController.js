f90c4e290a62033d9cef2ee28938c077e0b5c6e7
julien-alaria <julienalaria@gmail.com>
2026-03-04 16:47:27 +0100

    debug newsletter

--- back/src/controllers/NewsLetterController.js
+++ back/src/controllers/NewsLetterController.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-async function main() {
+
+async function main(req, res) {
     const client = new BrevoClient({
         apiKey: process.env.BREVO_KEY,
     });
-    await client.contacts.createContact({});
+    await client.contacts.createContact({email: "julien.alaria@laplateforme.io", 
+        listIds: [
+            4,
+        ],
+    });
+    res.status(200).json({message : "tout va bien"});
 }
 
 export default { main };
--- back/src/routes/NewsLetter.route.js
+++ back/src/routes/NewsLetter.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import NewsLetterController from "../controllers/NewsLetterController";
+import NewsLetterController from "../controllers/NewsLetterController.js";
 
 const newsLetterRouter = express.Router();
 
