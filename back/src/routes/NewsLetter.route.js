9f2342ea7bd8d0156081c875d73f1a8f484076a3
julien-alaria <julienalaria@gmail.com>
2026-03-06 11:33:58 +0100

    debug

--- back/src/routes/NewsLetter.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import NewsLetterController from "../controllers/NewsLetterController.js";
-
-const newsLetterRouter = express.Router();
-
-newsLetterRouter.post("/", NewsLetterController.main);
-
-export default newsLetterRouter;
